c6ed1c2502d28d08362fc2890affcd59db2f8640
Asfhen <joaopedro.sjc@gmail.com>
2025-02-13 22:37:17 -0300

    Project initialization

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+use bevy::utils::HashMap;
+use renet::ClientId;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct PlayerAttributes {
+    pub position: [f32; 3]
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum ClientMessage {
+    PlayerMove([f32; 3]),
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum ServerMessage {
+    LobbySync(HashMap<ClientId, PlayerAttributes>),
+    PlayerJoin(ClientId),
+    PlayerLeave(ClientId),
+}
